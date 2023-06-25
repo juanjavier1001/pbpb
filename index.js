3301768026513f530ec8d94ac7e26ad85137c3c1
juanjavier1001 <javierdeveloper1001@gmail.com>
2023-06-25 02:29:33 -0300

    hola jc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('hola ura');
+	res.send('hola jc');
 });
 
 app.listen(4000, console.log('server on ...'));
